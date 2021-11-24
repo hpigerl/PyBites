db1427a4705a5d03194cfdfaaa47874e73329667
holgerpigerl <h.pigerl@accure.net>
2021-11-24 14:12:20 +0100

    solve 153

---
+++ 153/rounding.py
@@ -0,0 +1,10 @@
+from typing import List
+import math
+
+
+def round_up_or_down(transactions: List[float], up=True):
+    """Round the list of transactions passed in.
+    If up=True (default) round up, else round down.
+    Return a new list of rounded values
+    """
+    return [math.ceil(num) if up else math.floor(num) for num in transactions]
