058bef52d3dbf8da48042583739cf78970a5e777
holgerpigerl <h.pigerl@accure.net>
2021-11-29 18:36:02 +0100

    make it a bit nicer

--- 138/zoo.py
+++ 138/zoo.py
@@ -1,20 +1,17 @@
 class Animal:
 
-    zoo_dict = {}
+    _zoo = {}
+    _last_id = 10000
 
     def __init__(self, name: str):
         self.name = name.title()
-
-        if len(self.__class__.zoo_dict) == 0:
-            self.id = 10001
-        else:
-            self.id = max(self.__class__.zoo_dict) + 1
-
-        self.__class__.zoo_dict[self.id] = self
+        self.id = self.__class__._last_id + 1
+        self.__class__._last_id = self.id
+        self.__class__._zoo[self.id] = self
 
     def __str__(self):
         return f"{self.id}. {self.name}"
 
     @classmethod
     def zoo(cls):
-        return "\n".join([animal.__str__() for animal in cls.zoo_dict.values()])
+        return "\n".join([str(animal) for animal in cls._zoo.values()])
