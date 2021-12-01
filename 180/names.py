a4289916c6bfc67f9babb9b4fbf9ddecaad6a170
holgerpigerl <h.pigerl@accure.net>
2021-12-01 14:43:01 +0100

    solve 180

---
+++ 180/names.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+# fake data from https://www.mockaroo.com
+data = """last_name,first_name,country_code
+Watsham,Husain,ID
+Harrold,Alphonso,BR
+Apdell,Margo,CN
+Tomblings,Deerdre,RU
+Wasielewski,Sula,ID
+Jeffry,Rudolph,TD
+Brenston,Luke,SE
+Parrett,Ines,CN
+Braunle,Kermit,PL
+Halbard,Davie,CN"""
+
+
+def group_names_by_country(data: str = data) -> defaultdict:
+    countries = defaultdict(list)
+    data = data.splitlines()[1:]
+    for entry in data:
+        last_name, first_name, country_code = entry.split(",")
+        countries[country_code].append(first_name + " " + last_name)
+    return countries
