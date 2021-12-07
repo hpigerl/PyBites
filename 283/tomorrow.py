40ee0053bd62c8a10ca185deb1b374bb94d4bf4c
holgerpigerl <h.pigerl@accure.net>
2021-12-07 09:36:53 +0100

    solve 283

---
+++ 283/tomorrow.py
@@ -0,0 +1,11 @@
+from datetime import date, datetime, timedelta
+from typing import Optional
+
+
+def tomorrow(date_today: Optional[datetime] = None):
+    if date_today is None:
+        date_today = datetime.today()
+    tmr = date_today + timedelta(days=1)
+    if hasattr(tmr, "hour"):
+        tmr = tmr.date()
+    return tmr
